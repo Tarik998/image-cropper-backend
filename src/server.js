2727de294835f7dc04adb451199fc2e9a5ad1039
Tarik <mehmedovictt@gmail.com>
2025-10-03 01:07:40 +0200

    import executeQuery from database module

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const sharp = require('sharp');
+const { executeQuery } = require('./database');
 require('dotenv').config();
 
 const app = express();
